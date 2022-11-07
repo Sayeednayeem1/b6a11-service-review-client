f62bb0f5e2e0a9b1642eba62f2df8626816fd21f
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-07 22:35:05 +0600

    installed tailwind daisyUI and react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>this isa  test
-        
-      </h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
