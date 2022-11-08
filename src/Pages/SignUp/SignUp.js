d7ea6d2318a0a5f0c44ed161396f9c062d675e69
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-08 12:44:56 +0600

    handled signUp method

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, } from 'react-router-dom';
 import login from '../../assets/Images/login.png';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const SignUp = () => {
 
+    const {createUser} = useContext(AuthContext);
 
     const handleSignUp = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then( result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch( error => console.error(error));
     }
     return (
         <div>
@@ -20,15 +32,21 @@ const SignUp = () => {
                         <form onSubmit={handleSignUp} className="card-body">
                             <div className="form-control">
                                 <label className="label">
+                                    <span className="label-text">Your Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required/>
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
                                 <input className='btn btn-primary' type="submit" value='Register' />
