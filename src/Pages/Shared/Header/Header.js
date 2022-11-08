341b2f1783e58c7d2b952836f470adc3da3c0236
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-08 10:07:24 +0600

    added login form

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="navbar bg-primary text-primary-content p-5">
-            <a href='/' className="btn btn-ghost normal-case text-xl">daisyUI</a>
-            <Link className='font-bold text-2xl mr-4' to='/home'>Home</Link>
-            <Link className='font-bold text-2xl mr-4' to='/blog'>Blog</Link>
+            <Link to='/' className="font-bold text-2xl mr-6 btn btn-outline btn-accent border-none">Dr. Chowdhury Walid</Link>
+            <Link className='font-semibold text-2xl mr-6 btn btn-outline btn-success border-none' to='/home'>Home</Link>
+            <Link className='font-semibold text-2xl mr-6 btn btn-outline btn-success border-none' to='/blog'>Blog</Link>
+            <Link className='font-semibold text-2xl mr-6 btn btn-outline btn-success border-none' to='/login'>login</Link>
+            <Link className='font-semibold text-2xl mr-6 btn btn-outline btn-success border-none' to='/signup'>SignUp</Link>
         </div>
     );
 };
