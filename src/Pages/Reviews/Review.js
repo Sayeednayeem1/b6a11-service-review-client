36504a6160fb0787d79e4be71c7335e8beb2d270
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-10 09:25:04 +0600

    added delete method

---
+++ src/Pages/Reviews/Review.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+const Review = ({order}) => {
+    const {serviceName,_id, price, customer, email, comment, ratings} = order;
+    const [orderService, setService] = useState({});
+
+    // useEffect(() =>{
+    //     fetch('http://localhost:5000/services/${service}')
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    // }, [])
+
+
+
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you sure you wanna delete? this Order?');
+        if(proceed){
+            fetch(`http://localhost:5000/orders/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+        }
+    }
+    
+    
+    return (
+        <tr>
+            <th>
+                <label>
+                    <button onClick={() => handleDelete(_id)} className='btn btn-ghost'>X</button>
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    
+                    <div>
+                        <div className="font-bold">{serviceName}</div>
+                        <div className="text-sm opacity-50">${price}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {customer}
+                <br />
+                <span className="badge badge-ghost badge-sm">{comment}</span>
+            </td>
+            <td>Ratings: {ratings}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">{email}</button>
+            </th>
+        </tr>
+    );
+};
+
+export default Review;
--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,20 +1,43 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import Review from './Review';
 
 const Reviews = () => {
     const { user } = useContext(AuthContext);
-    const [orders, setOrders] = useState({});
+    const [orders, setOrders] = useState([]);
 
-    useEffect(() =>{
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setOrders(data))
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
     }, [user?.email])
-    
+
 
     return (
-        <div>
-            <h3>you have: {orders?.length}</h3>
+        <div className='container mx-auto mt-20 mb-20'>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>Your Order</th>
+                            <th>Name</th>
+                            <th>Ratings</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <Review
+                            
+                                key={order._id}
+                                order={order}
+                            
+                            ></Review>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
